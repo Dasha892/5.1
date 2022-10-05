8a8de4e2473dec88601c5e41b3a9b7b77d68601e
HP <HP@WIN-8VMT11N5JJN>
2022-10-06 01:28:45 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 5; // ввод целочисленной переменной a
+        int b = 10; // ввод целочисленной переменной b
+        int c = 21; // ввод целочисленной переменной c
+
+        Console.WriteLine(a); // вывод на экран строки "..."
+        Console.WriteLine(b); // вывод на экран строки "..."
+        Console.WriteLine(c); // вывод на экран строки "..."
+    }
+}
